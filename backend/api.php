6d51a4b46f060ed0a364b750e2831e2e8d6779c1
unknown <jhi002247@gmail.com>
2025-03-21 12:26:31 -0400

    deleted comment

--- backend/api.php
+++ backend/api.php
@@ -4,7 +4,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// Handle OPTIONS requests (pre-flight request)
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
